ccf5393688340a8f0228374e256230635b63bb4f
Jakob Kaltmann <fake@email.com>
2022-12-01 11:45:42 +0100

    day1

---
+++ Sources/aoc2022/aoc2022.swift
@@ -0,0 +1,45 @@
+import Foundation
+
+@main
+public struct aoc2022 {
+    public static func main() {
+        
+        if let filepath = Bundle.module.path(forResource:"01_input", ofType:"txt") {
+            do {
+                let filecontent = try String(contentsOfFile: filepath)
+                
+                var elves: [[Int]] = []
+                var current: [Int] = []
+                for calorieline in filecontent.components(separatedBy: "\n") {
+                    if calorieline == "" {
+                        elves.append(current)
+                        current = []
+                        continue
+                    }
+                    let calorieInt = Int(calorieline) ?? 0
+                    current.append(calorieInt)
+                }
+
+                let summed = elves
+                    .compactMap { $0.reduce(0) {$0 + $1} }
+                    .max()
+                
+                let topThree = elves
+                    .compactMap { $0.reduce(0) {$0 + $1} }
+                    .sorted { $0 > $1 }
+                    .prefix(3)
+                    .reduce(0) { $0 + $1 }
+                
+                
+                print(summed ?? 0)
+                print (topThree)
+            }catch  {
+                print("error")
+            }
+        }
+        
+        //        print(aoc2022().text)
+    }
+    
+    
+}
