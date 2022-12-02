e556b1d369a2b12340365b3e6664d9b89433167c
Jakob Kaltmann <fake@email.com>
2022-12-02 10:36:45 +0100

    day2 star2

--- Sources/aoc2022/aoc2022.swift
+++ Sources/aoc2022/aoc2022.swift
@@ -1,18 +1,12 @@
 import Foundation
 
 
-
-let v: [String:Int] = [
-    "X": 1,
-    "Y": 2,
-    "Z": 3,
-]
-
 @main
 public struct aoc2022 {
     public static func main() {
         //        day1()
         day2()
+        day2_star2()
     }
     
     static func day2() {
@@ -25,7 +19,27 @@ public struct aoc2022 {
                     let opponentMove = gameline.components(separatedBy: " ")[0]
                     let yourMove = gameline.components(separatedBy: " ")[1]
                     score += haveYouWonRPS(opponentMove, yourMove).rawValue
-                    score += v[yourMove] ?? 0
+                    score += rpsVal[toRPSMove(yourMove)] ?? 0
+                }
+                
+                print(score)
+            } catch {
+                print(error)
+            }
+        }
+    }
+    
+    static func day2_star2() {
+        if let filepath = Bundle.module.path(forResource:"02_input", ofType:"txt") {
+            do {
+                let filecontent = try String(contentsOfFile: filepath)
+                
+                var score = 0
+                for gameline in filecontent.components(separatedBy: "\n") {
+                    let opponentMove = toRPSMove(gameline.components(separatedBy: " ")[0])
+                    let suggestedResult = toRPSResult(gameline.components(separatedBy: " ")[1])
+                    score += suggestedResult.rawValue
+                    score += rpsVal[youSouldPlay(opponentMove, suggestedResult)] ?? 0
                 }
                 
                 print(score)
@@ -75,10 +89,82 @@ public struct aoc2022 {
     }
 }
 
+enum RPSMove {
+    case rock
+    case paper
+    case scissors
+    case unknown
+}
+
+let rpsVal: [RPSMove:Int] = [
+    .rock: 1,
+    .paper: 2,
+    .scissors: 3,
+]
+
+func toRPSMove(_ s: String) -> RPSMove {
+    switch s {
+    case "A", "X":
+        return .rock
+    case "B", "Y":
+        return .paper
+    case "C", "Z":
+        return .scissors
+    default:
+        return .unknown
+    }
+}
+
+
 enum RPSResult: Int {
     case won = 6
     case tie = 3
     case lost = 0
+}
+
+func toRPSResult(_ s:String) -> RPSResult {
+    switch s {
+    case "Z": return .won
+    case "Y": return .tie
+    default: return .lost
+    }
+}
+
+
+let rpsRequirement: [String:RPSResult] = [
+    "X": .lost,
+    "Y": .tie,
+    "Z": .won
+]
+
+
+func youSouldPlay(_ opp: RPSMove, _ expectedResult: RPSResult) -> RPSMove {
+    switch (opp, expectedResult) {
+        
+    case (.rock, .won):
+        return .paper
+    case (.rock, .lost):
+        return .scissors
+    case (.rock, .tie):
+        return .rock
+        
+    case (.paper, .won):
+        return .scissors
+    case (.paper, .lost):
+        return .rock
+    case (.paper, .tie):
+        return .paper
+        
+    case (.scissors, .won):
+        return .rock
+    case (.scissors, .lost):
+        return .paper
+    case (.scissors, .tie):
+        return .scissors
+        
+    default:
+        return .unknown
+    }
 }
 
 func haveYouWonRPS(_ opp: String,_ you: String) -> RPSResult {
